cb9731bfd067a1565da0d5c31e4a38bc4ac2fcb2
10031066 <edgar_5_11@hotmail.com>
2014-03-25 22:31:34 -0600

    Animales Dociles

---
+++ JavaApplication2Zoooologico/src/javaapplication2zoooologico/Dociles.java
@@ -0,0 +1,33 @@
+package javaapplication2zoooologico;
+public class Dociles extends Animales{
+
+    public Dociles(int indice) {
+        switch (indice){
+            case 1: //chango
+                crear(indice,"Chango",false,60,0,250,false,true,false,true);
+                break;
+            case 2://elefante
+                crear(indice,"Elefante",false,250,0,750,true,true,false,false);
+                break;
+            case 3://jirafa
+                crear(indice,"Jirafa",false,200,0,650,true,true,false,false);
+                break;
+            case 4: // pingüino
+                crear(indice,"Pingüino",false,100,0,400,false,true,true,false);
+                break;
+            case 5: //cebra
+                crear(indice,"Cebra",false,130,0,500,true,true,false,false);
+                break;
+            case 6: //camello
+                crear(indice,"Camello",false,180,0,600,true,true,false,false);
+                break;
+            case 7: //Foca
+                crear(indice,"Foca",false,110,0,450,false,true,true,false);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    
+}
