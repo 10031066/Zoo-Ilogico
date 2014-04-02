f422a89d8c7d37ded284107faa8a3ca83c9181bb
10031066 <edgar_5_11@hotmail.com>
2014-04-01 23:29:06 -0600

    Rejas

---
+++ JavaApplication2Zoooologico/src/javaapplication2zoooologico/reja.java
@@ -0,0 +1,25 @@
+package javaapplication2zoooologico;
+public class reja {
+
+    int tipo;
+    int Resistencia;
+    int ResistenciaMax;
+    int iniI,iniJ;
+    int finI,finJ;
+    
+    public reja(int tipo, int iniI, int iniJ, int finI, int finJ) {
+        this.tipo = tipo;
+        this.iniI = iniI;
+        this.iniJ = iniJ;
+        this.finI = finI;
+        this.finJ = finJ;
+        int res=0;
+        if(tipo==1){
+            //establece la resisistenciaMax del tipo 1
+        }else{
+            //establece la resisistenciaMax del tipo 2
+        }
+        this.Resistencia= res;
+        this.ResistenciaMax=res;
+    }
+}
